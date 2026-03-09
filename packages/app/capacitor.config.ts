4cccdc36af8557f91980143c7937018d23b6ae7f
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-09 22:49:36 +0700

    update cap

--- packages/app/capacitor.config.ts
+++ packages/app/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.github.hieudoanm.kaprekar',
-  appName: '@kaprekar/app',
+  appName: 'Kaprekar',
   webDir: '../../docs',
   android: { path: 'mobile/android' },
   ios: { path: 'mobile/ios' },
